5b10c030c2ce4e4b3f7a9c15bb6f775b2f09717e
Subhrojit10 <59506122+Subhrojit10@users.noreply.github.com>
2025-05-04 18:37:04 +0530

    Update personal-information.component.ts

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -9,14 +9,14 @@ export class PersonalInformationComponent implements OnInit {
   myData: string[][] = [
     ['Name', 'Subhrojit Bhattacharjee'],
     ['DOB', '22/02/1998'],
-    ['Work Exp', '3.6 Years'],
+    ['Work Exp', '5.2 Years'],
     ['Education', 'BTech'],
     ['Interests', 'Cricket, Web Development'],
   ];
 
   aboutMe: string[] = [
-    'Hi, I am a Software Engineer with 3.6+ years of experience in software industry.',
-    'Worked as a Frontend Developer in TCS for devoping Web Applications using technologies such as(Angular 7-14, NgRx store, Material design, Bootstrap, JavaScript, HTML5 and CSS3.).',
+    'Hi, I am a Software Engineer with 5.2+ years of experience in software industry.',
+    'Worked as a Fullstack Developer in TCS for devoping Web Applications using technologies such as(Angular 7-14, NgRx store, Material design, Bootstrap, JavaScript, HTML5 and CSS3.).',
     'Delivered all product withing deadlines. Always eager to learn new technologies.',
     'Currently, Leading a team of 2 Junior Developer for developing a new module',
   ];
